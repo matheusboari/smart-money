b78aff6e68a90a24b098e2d3005907c81811ff36
Matheus Boari <matheusboari@gmail.com>
2019-05-29 13:10:48 -0300

    Header of wallet created

--- client/src/views/Wallet/wallet.vue
+++ client/src/views/Wallet/wallet.vue
@@ -1,6 +1,46 @@
 <template>
-    <div class="profile-wrapper">
+    <div class="wallets-header-wrapper">
         <primary-header tab="wallet" />
+        <div class="wallets-wrapper">
+           <div class="wallets">
+               <div class="add-wallet">
+                   <i class="el-icon-wallet icon"></i>
+                   <h3 class="title">Adicionar Carteira</h3>
+               </div>
+               <div class="new-wallet">
+                   <i class="el-icon-wallet icon"></i>
+                   <h3 class="title">New Wallet</h3>
+               </div>
+               <div class="new-wallet">
+                   <i class="el-icon-wallet icon"></i>
+                   <h3 class="title">New Wallet</h3>
+               </div>
+               <div class="new-wallet">
+                   <i class="el-icon-wallet icon"></i>
+                   <h3 class="title">New Wallet</h3>
+               </div>
+               <div class="new-wallet">
+                   <i class="el-icon-wallet icon"></i>
+                   <h3 class="title">New Wallet</h3>
+               </div>
+               <div class="new-wallet">
+                   <i class="el-icon-wallet icon"></i>
+                   <h3 class="title">New Wallet</h3>
+               </div>
+               <div class="new-wallet">
+                   <i class="el-icon-wallet icon"></i>
+                   <h3 class="title">New Wallet</h3>
+               </div>
+               <div class="new-wallet">
+                   <i class="el-icon-wallet icon"></i>
+                   <h3 class="title">New Wallet</h3>
+               </div>
+               <div class="new-wallet">
+                   <i class="el-icon-wallet icon"></i>
+                   <h3 class="title">New Wallet</h3>
+               </div>
+           </div>
+        </div>
     </div>
 </template>
 
@@ -15,5 +55,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '~styles/settings/variables/colors';
 
+.wallets-wrapper {
+    width: 100%;
+    height: 100%;
+    color: $defaultFont;
+    >.wallets {
+        display: flex;
+        padding: 20px 50px;
+        background-color: $gray-300;
+        overflow-x: auto;
+        -webkit-overflow-scrolling: touch;
+        -ms-overflow-style: -ms-autohiding-scrollbar;
+        >.add-wallet {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            min-width: 200px;
+            max-width: 200px;
+            padding: 20px;
+            border: 3px dashed rgba(51, 51, 51, .2);
+            border-radius: 4px;
+            flex-grow: 1;
+            cursor: pointer;
+            >.icon {
+                font-size: 1.5em;
+            }
+        }
+        >.new-wallet {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            min-width: 200px;
+            max-width: 200px;
+            margin-left: 20px;
+            padding: 20px;
+            border: 3px solid rgba(51, 51, 51, .2);
+            border-radius: 4px;
+            cursor: pointer;
+            flex-grow: 1;
+            >.icon {
+                font-size: 1.5em;
+            }
+        }
+    }
+}
 </style>
