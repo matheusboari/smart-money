0281fa4de16cd01616e6b541f9dca8760716b8f3
Matheus Boari <matheusboari@gmail.com>
2019-05-29 12:46:02 -0300

    Finished Calculator Calculation

--- client/src/views/Calculator/calculator.vue
+++ client/src/views/Calculator/calculator.vue
@@ -31,12 +31,12 @@
                                 <template slot="append">% ao ano</template>
                             </el-input>
                         </div>
-                        <div class="input ipca-tax">
+                        <!-- <div class="input ipca-tax">
                             <strong class="title">Taxa IPCA</strong>
                             <el-input placeholder="6,4" v-model="taxIpca">
                                 <template slot="append">% ao ano</template>
                             </el-input>
-                        </div>
+                        </div> -->
                         <div class="input cdb-lc">
                             <strong class="title">CDB/LC</strong>
                             <el-input placeholder="83" v-model="cdb">
@@ -62,31 +62,30 @@
                     <div class="results">
                         <div class="result poupanca">
                             <strong class="title">Poupança</strong>
-                            <span class="stat">Valor líquido: R$ 4.55</span>
+                            <span class="stat">Valor líquido: R$ {{profit.poupanca}}</span>
                             <el-progress :text-inside="false" :stroke-width="18" :percentage="4.5" color="rgba(142, 113, 199, 0.7)"></el-progress>
                         </div>
                         <div class="result">
                             <strong class="title">CDB</strong>
-                            <span class="stat">Valor Total: R$ 5.31</span>
+                            <span class="stat">Valor Total: R$ {{profit.cdb.total}}</span>
                             <div class="imposto">
-                                <el-badge :value="taxes + '%'" class="item"><span class="stat">Imposto de Renda: R$ 1.06</span></el-badge>
+                                <el-badge :value="taxes + '%'" class="item"><span class="stat">Imposto de Renda: R$ {{profit.cdb.taxes}}</span></el-badge>
                             </div>
-                            <span class="stat">Valor Líquido: R$ 4.25</span>
+                            <span class="stat">Valor Líquido: R$ {{profit.cdb.amount}}</span>
                             <el-progress :text-inside="false" :stroke-width="18" :percentage="4.3" color="rgba(142, 113, 199, 0.7)"></el-progress>
                         </div>
                         <div class="result">
                             <strong class="title">LCI</strong>
-                            <span class="stat">Valor Líquido: R$ 5.82</span>
+                            <span class="stat">Valor Líquido: R$ {{profit.lci}}</span>
                             <el-progress :text-inside="false" :stroke-width="18" :percentage="5.8" color="rgba(142, 113, 199, 0.7)"></el-progress>
                         </div>
                         <div class="result">
                             <strong class="title">Tesouro SELIC</strong>
-                            <span class="stat">Valor Total: R$ 6.40</span>
+                            <span class="stat">Valor Total: R$ {{profit.selic.total}}</span>
                             <div class="imposto">
-                                <el-badge :value="taxes + '%'" class="item"><span class="stat">Imposto de Renda: R$ 1.28</span></el-badge>
+                                <el-badge :value="taxes + '%'" class="item"><span class="stat">Imposto de Renda: R$ {{profit.selic.taxes}}</span></el-badge>
                             </div>
-                            <span class="stat">Taxa Bovespa: R$ 0.30</span>
-                            <span class="stat">Valor Líquido: R$ 4.82</span>
+                            <span class="stat">Valor Líquido: R$ {{profit.selic.amount}}</span>
                             <el-progress :text-inside="false" :stroke-width="18" :percentage="4.8" color="rgba(142, 113, 199, 0.7)"></el-progress>
                         </div>
                     </div>
@@ -112,15 +111,56 @@ export default {
             cdb: 83,
             licLca: 91,
             taxIpca: 6.4,
-            taxes: 20
+            taxes: 20,
+            profit: {
+                poupanca: 4.55,
+                cdb: {
+                    total: 5.31,
+                    taxes: 1.06,
+                    amount: 4.25
+                },
+                lci: 5.82,
+                selic: {
+                    total: 6.4,
+                    taxes: 1.28,
+                    amount: 4.82
+                }
+            }
         }
     },
     methods: {
-
+        MonthTax (yearlyTax) {
+            return Math.pow((1 + (yearlyTax / 100)), (1/12)) - 1
+        },
+        CompoundInterest (initialValue, monthTax, months) {
+            return initialValue * Math.pow((1 + monthTax), months)
+        },
+        PoupancaProfit (initialValue, selic, months) {
+            if(selic <= 8.5) selic = selic * 0.7
+            this.profit.poupanca = (this.CompoundInterest(initialValue, this.MonthTax(selic), months) - initialValue).toFixed(2)
+        },
+        CdbProfit (initialValue, cdb, di, months, taxes) {
+            let amount = this.CompoundInterest(initialValue, this.MonthTax(cdb * di / 100), months)
+            this.profit.cdb.total = (amount - initialValue).toFixed(2)
+            this.profit.cdb.taxes = (this.profit.cdb.total * (taxes / 100)).toFixed(2)
+            this.profit.cdb.amount = (this.profit.cdb.total - this.profit.cdb.taxes).toFixed(2)
+        },
+        LciProfit (initialValue, lci, di, months) {
+            this.profit.lci = (this.CompoundInterest(initialValue, this.MonthTax(lci * di / 100), months) - initialValue).toFixed(2)
+        },
+        SelicProfit (initialValue, selic, months, taxes) {
+            let amount = this.CompoundInterest(initialValue, this.MonthTax(selic), months)
+            this.profit.selic.total = (amount - initialValue).toFixed(2)
+            this.profit.selic.taxes = (this.profit.selic.total * (taxes / 100)).toFixed(2)
+            this.profit.selic.amount = (this.profit.selic.total - this.profit.selic.taxes).toFixed(2)
+        }
     },
     watch: {
         value () {
-            
+            this.PoupancaProfit(Number(this.value), Number(this.taxSelic), Number(this.months))
+            this.CdbProfit(Number(this.value), Number(this.cdb), Number(this.taxDi), Number(this.months), this.taxes)
+            this.LciProfit(Number(this.value), Number(this.licLca), Number(this.taxDi), Number(this.months))
+            this.SelicProfit(Number(this.value), Number(this.taxSelic), Number(this.months), this.taxes)
         },
         months () {
             if(this.months <= 6) this.taxes = 22.5 
@@ -128,6 +168,25 @@ export default {
             else if (this.months > 12 && this.months <= 24) this.taxes = 17.5
             else if (this.months > 24) this.taxes = 15
             else this.taxes = 20
+            
+            this.PoupancaProfit(Number(this.value), Number(this.taxSelic), Number(this.months))
+            this.CdbProfit(Number(this.value), Number(this.cdb), Number(this.taxDi), Number(this.months), this.taxes)
+            this.LciProfit(Number(this.value), Number(this.licLca), Number(this.taxDi), Number(this.months))
+            this.SelicProfit(Number(this.value), Number(this.taxSelic), Number(this.months), this.taxes)
+        },
+        taxSelic () {
+            this.PoupancaProfit(Number(this.value), Number(this.taxSelic), Number(this.months))
+            this.SelicProfit(Number(this.value), Number(this.taxSelic), Number(this.months), this.taxes)
+        },
+        taxDi () {
+            this.CdbProfit(Number(this.value), Number(this.cdb), Number(this.taxDi), Number(this.months), this.taxes)
+            this.LciProfit(Number(this.value), Number(this.licLca), Number(this.taxDi), Number(this.months))
+        },
+        cdb () {
+            this.CdbProfit(Number(this.value), Number(this.cdb), Number(this.taxDi), Number(this.months), this.taxes)
+        },
+        licLca () {
+            this.LciProfit(Number(this.value), Number(this.licLca), Number(this.taxDi), Number(this.months))
         }
     }
 }
@@ -142,6 +201,7 @@ export default {
         max-width: 1050px;
         margin: auto;
         margin-top: 50px;
+        margin-bottom: 50px;
         >.calculator {
             display: flex;
             align-items: flex-start;
